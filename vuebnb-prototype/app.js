f1886c99373c3ed499b54c07679d80dfe4a72489
tavo1987 <tavo198718@gmail.com>
2018-01-19 17:10:03 -0500

    Add modal window

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -10,5 +10,34 @@ var app =new Vue({
         amenities: sample.amenities,
         prices: sample.prices,
         contracted: true,
+        modalOpen: false,
+    },
+    methods: {
+        escapeKeyListener: function(event) {
+            console.log(event.keyCode);
+            if (event.keyCode == 27 && this.modalOpen) {
+                this.modalOpen = false;
+            }
+        }
+    },
+
+    watch: {
+        modalOpen: function() {
+            var className = 'modal-open';
+            if (this.modalOpen) {
+                document.body.classList.add(className);
+            }else {
+                document.body.classList.remove(className);
+            }
+        }
+    },
+
+    created: function() {
+        document.addEventListener('keyup', this.escapeKeyListener);
+    },
+
+    destroyed:function(){
+        console.log('destroyed');
+        document.removeEventListener('keyup',this.escapeKeyListener);
     }
 });
